fb6063dc65d3fbfbc803f4a3ba9c5bc7208914fd
michelle nderi <nderimichelle@gmail.com>
2023-05-25 22:06:33 +0300

    Complete assignment

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name,age,sex,color,temperament,alive)VALUES
+("Mr. Chocolate", 20, "M", "Dark brown", "calm", 0),
+("Rowdy", 10, "M", "Black", "intense", 1),
+("Tabitha", 6, "F", "Dark brown", "Nice", 1),
+("Sargeant Brown", 19, "M", "White", "Slimy", 0),
+("Melissa", 13, "F", "dark brown", "goofy", 1),
+("Grinch", 2, "M", "Black", "Grinchy", 1),
+("Wendy", 6, "F", "Hazel", "naive", 1),
+(null, 20, "M", "Black", "aggressive", 0);
